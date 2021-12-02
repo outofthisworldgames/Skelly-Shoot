108620d85aaeb6690cf1289978b2b8b8d26f8594
EvanBennett0 <erbennett2022@gmail.com>
2021-12-02 09:29:09 -0800

    Main Menu Update

--- team project prototype 1/Assets/scripts/GameManager.cs
+++ team project prototype 1/Assets/scripts/GameManager.cs
@@ -16,6 +16,7 @@ public class GameManager : MonoBehaviour
     public GameObject quitButton;
     public GameObject restartButton;
     public GameObject pauseIndicator;
+    public GameObject playButton;
     public int enemyCounter;
 
     public bool playerOnEnd = false;
@@ -41,7 +42,7 @@ public class GameManager : MonoBehaviour
             
             
         }
-        Debug.Log("Test");
+       // Debug.Log("Test");
         if (playerOnEnd == true && enemyCounter == 0)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
